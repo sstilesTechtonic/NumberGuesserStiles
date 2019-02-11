98810822f559ef9de2f26240be6f6a54b934844f
steven_stiles <steven.stiles1@gmail.com>
2019-02-11 01:22:39 -0700

    Commit with Hello World working

---
+++ NumberGuesserStiles/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Namespace
+
+namespace NumberGuesserStiles
+{
+    //Main Class
+    class Program
+    {
+        //Entry Point Method
+        static void Main(string[] args)
+        {
+            // Start Here //
+            Console.WriteLine("Hello World");
+        }
+    }
+}
+
